912f99e1346f9e9e3853943e306ef05a3de1310e
nfunderburgh <thunderquack205@gmail.com>
2024-06-30 18:01:12 -0700

    Christian Crush page

--- BibleApp/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace BibleApp.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
